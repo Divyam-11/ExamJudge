cc44754e9c67574137e9313cf01ebd1ade46e9af
AkshatSarin2004 <akshatsarin3461@gmail.com>
2025-11-13 17:53:37 +0530

    changes

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,7 @@
 import google.generativeai as genai
 
 # Configure Gemini
-genai.configure(api_key="AIzaSyCKMMnYdEgyqu3zT45oMhWHiffUe34svLw")
+genai.configure(api_key="enter api here")
 
 # Choose a fast, smart model
 model = genai.GenerativeModel("gemini-2.5-flash")
